9d43ee3197fceb2fc56f89e3e76467fe35f568e9
Bejussi <pindooqy@gmail.com>
2023-06-06 21:37:25 +0300

    set notification time, language, theme; show privacy, terms, contact, rate

--- app/src/main/java/com/bejussi/dailyadvice/presentation/MainActivity.kt
+++ app/src/main/java/com/bejussi/dailyadvice/presentation/MainActivity.kt
@@ -3,6 +3,8 @@ package com.bejussi.dailyadvice.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.bejussi.dailyadvice.R
 import com.bejussi.dailyadvice.presentation.notification.AlarmScheduler
@@ -21,6 +23,25 @@ class MainActivity : AppCompatActivity() {
 
         settingsViewModel.getNotificationTime.observe(this) { time ->
             setAlarm(time)
+        }
+
+        settingsViewModel.getTheme.observe(this@MainActivity) { themeMode ->
+            when (themeMode) {
+                "light" -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+                "dark" -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+                "system" -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                }
+            }
+        }
+
+        settingsViewModel.getLanguage.observe(this@MainActivity) { language ->
+            val localeList = LocaleListCompat.forLanguageTags(language)
+            AppCompatDelegate.setApplicationLocales(localeList)
         }
     }
 
--- app/src/main/java/com/bejussi/dailyadvice/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/com/bejussi/dailyadvice/presentation/settings/SettingsFragment.kt
@@ -1,17 +1,34 @@
 package com.bejussi.dailyadvice.presentation.settings
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.bejussi.dailyadvice.R
+import com.bejussi.dailyadvice.core.util.makeToast
 import com.bejussi.dailyadvice.databinding.FragmentSettingsBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment(), View.OnClickListener {
 
     private lateinit var binding: FragmentSettingsBinding
+    private val settingsViewModel: SettingsViewModel by viewModels()
+
+    private var selectedThemeIndex = 0
+    private var selectedLanguageIndex = 0
+    private var selectedTime = "12:00"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,6 +51,47 @@ class SettingsFragment : Fragment(), View.OnClickListener {
         binding.contactButton.setOnClickListener(this)
         binding.privacyPolicyButton.setOnClickListener(this)
         binding.termsConditionsButton.setOnClickListener(this)
+
+        settingsViewModel.getTheme.observe(viewLifecycleOwner) { themeMode ->
+            when (themeMode) {
+                "light" -> {
+                    selectedThemeIndex = 0
+                }
+                "dark" -> {
+                    selectedThemeIndex = 1
+                }
+                "system" -> {
+                    selectedThemeIndex = 2
+                }
+            }
+        }
+
+        settingsViewModel.getLanguage.observe(viewLifecycleOwner) { language ->
+            when (language) {
+                "en" -> {
+                    selectedLanguageIndex = 0
+                }
+                "uk" -> {
+                    selectedLanguageIndex = 1
+                }
+                "ru" -> {
+                    selectedLanguageIndex = 2
+                }
+                "de" -> {
+                    selectedLanguageIndex = 3
+                }
+                "fr" -> {
+                    selectedLanguageIndex = 4
+                }
+                "es" -> {
+                    selectedLanguageIndex = 5
+                }
+            }
+        }
+
+        settingsViewModel.getNotificationTime.observe(viewLifecycleOwner) { time ->
+            selectedTime = time
+        }
     }
 
     override fun onClick(v: View) {
@@ -51,31 +109,139 @@ class SettingsFragment : Fragment(), View.OnClickListener {
     }
 
     private fun showTerms() {
-        TODO("Not yet implemented")
+        val action = SettingsFragmentDirections.actionSettingsFragmentToWebFragment("terms")
+        findNavController().navigate(action)
     }
 
     private fun showPrivacy() {
-        TODO("Not yet implemented")
+        val action = SettingsFragmentDirections.actionSettingsFragmentToWebFragment("privacy")
+        findNavController().navigate(action)
     }
 
     private fun contactUs() {
-        TODO("Not yet implemented")
+        val intent = Intent(Intent.ACTION_SENDTO)
+            .setData(Uri.parse("mailto:?subject=DailyAdvice&body=Feedback for DailyAdvice&to=bejussiapp@gmail.com"))
+        if (intent.resolveActivity(requireContext().packageManager) != null) {
+            startActivity(intent)
+        } else {
+            requireContext().makeToast(getString(R.string.application_not_found))
+        }
     }
 
     private fun rateUs() {
-        TODO("Not yet implemented")
+        val appPackageName = requireActivity().packageName
+        try {
+            val intent = Intent(Intent.ACTION_VIEW,
+                Uri.parse("market://details?id=$appPackageName")
+            )
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            val intent = Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://play.google.com/store/apps/details?id=$appPackageName")
+            )
+            startActivity(intent)
+        }
     }
 
     private fun setTheme() {
-        TODO("Not yet implemented")
+        val themes = resources.getStringArray(R.array.themes)
+
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(R.string.select_theme))
+            .setSingleChoiceItems(themes, selectedThemeIndex) { dialog_, which ->
+                selectedThemeIndex = which
+            }
+            .setPositiveButton(getString(R.string.ok)) { dialog, which ->
+                setTheme(selectedThemeIndex)
+            }
+            .setNegativeButton(getString(R.string.cancel)) { dialog, which ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun setTheme(selectedThemeIndex: Int) {
+        lifecycleScope.launch {
+            when (selectedThemeIndex) {
+                0 -> {
+                    settingsViewModel.setTheme("light")
+                }
+                1 -> {
+                    settingsViewModel.setTheme("dark")
+                }
+                2 -> {
+                    settingsViewModel.setTheme("system")
+                }
+            }
+        }
     }
 
     private fun setLanguage() {
-        TODO("Not yet implemented")
+        val languages = resources.getStringArray(R.array.language)
+
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(R.string.select_language))
+            .setSingleChoiceItems(languages, selectedLanguageIndex) { dialog_, which ->
+                selectedLanguageIndex = which
+            }
+            .setPositiveButton(getString(R.string.ok)) { dialog, which ->
+                setLanguage(selectedLanguageIndex)
+            }
+            .setNegativeButton(getString(R.string.cancel)) { dialog, which ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun setLanguage(selectedLanguageIndex: Int) {
+        lifecycleScope.launch {
+            when (selectedLanguageIndex) {
+                0 -> {
+                    settingsViewModel.setLanguage("en")
+                }
+                1 -> {
+                    settingsViewModel.setLanguage("uk")
+                }
+                2 -> {
+                    settingsViewModel.setLanguage("ru")
+                }
+                3 -> {
+                    settingsViewModel.setLanguage("de")
+                }
+                4 -> {
+                    settingsViewModel.setLanguage("fr")
+                }
+                5 -> {
+                    settingsViewModel.setLanguage("es")
+                }
+            }
+        }
     }
 
     private fun setReminder() {
-        TODO("Not yet implemented")
+        val (hours, min) = selectedTime.split(":").map { it.toInt() }
+
+        val materialTimePicker = MaterialTimePicker.Builder()
+            .setTitleText(getString(R.string.select_time))
+            .setHour(hours)
+            .setMinute(min)
+            .setTimeFormat(TimeFormat.CLOCK_24H)
+            .build()
+
+        materialTimePicker.show(requireActivity().supportFragmentManager, "TIME_PICKER")
+
+        materialTimePicker.addOnPositiveButtonClickListener {
+            val pickedMinute: Int = materialTimePicker.minute
+
+            val formattedTime = if (pickedMinute < 10) {
+                "${materialTimePicker.hour}:0${materialTimePicker.minute}"
+            } else {
+                "${materialTimePicker.hour}:${materialTimePicker.minute}"
+            }
+
+            settingsViewModel.setNotificationTime(formattedTime)
+        }
+
     }
 
     private fun manageSubscription() {
